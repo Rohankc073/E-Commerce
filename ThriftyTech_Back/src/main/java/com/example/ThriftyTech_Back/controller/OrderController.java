ebcf2328ded4cf37afbc1c6dbb6edc69a3de07eb
Rohankc073 <125729603+Rohankc073@users.noreply.github.com>
2023-12-14 19:11:48 +0545

    OrderAdded

--- ThriftyTech_Back/src/main/java/com/example/ThriftyTech_Back/controller/OrderController.java
+++ ThriftyTech_Back/src/main/java/com/example/ThriftyTech_Back/controller/OrderController.java
@@ -1,4 +1,34 @@
 package com.example.ThriftyTech_Back.controller;
 
+import com.example.ThriftyTech_Back.pojo.OrderPojo;
+import com.example.ThriftyTech_Back.pojo.ProductPojo;
+import com.example.ThriftyTech_Back.service.OrderService;
+import com.example.ThriftyTech_Back.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/order")
 public class OrderController {
+    private final ProductService productService;
+    @GetMapping("/vieworder")
+    public String getOrder(Model model) {
+        model.addAttribute("order", new OrderPojo());
+//        return ma page ko naam ma
+        return "";
+    }
+    @PostMapping("/deleteorder")
+    public String deleteOrder(@Valid OrderPojo orderPojo) throws IOException {
+        OrderService.save(orderPojo);
+//        save vayesi kata redirect garne tesko naam : paxi
+        return "redirect:";
+    }
 }
