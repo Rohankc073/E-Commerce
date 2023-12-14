092bbb9ee25b21d13698cd2b10e67fb703453571
Rohankc073 <125729603+Rohankc073@users.noreply.github.com>
2023-12-14 10:53:13 +0545

    Delete ThriftyTech_Back directory

--- ThriftyTech_Back/src/main/java/com/example/ThriftyTech_Back/repo/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.ThriftyTech_Back.repo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.ThriftyTech_Back.entity.User;
-
-@Repository
-public interface UserRepo extends JpaRepository<User, Long> {
-}
