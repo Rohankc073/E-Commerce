7415386ef0f61efbaff0127c12e038ad6216f20a
Rohankc073 <125729603+Rohankc073@users.noreply.github.com>
2023-12-17 20:27:20 +0545

    Few frontend changes

--- Thrifty_Front/frontend/src/index.js
+++ Thrifty_Front/frontend/src/index.js
@@ -13,7 +13,7 @@ import Product from "./pages/product";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Product />
+    <HomePage />
   </React.StrictMode>
 );
 
