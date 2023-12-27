fec6a510293eff93d06be06fa385f9f3aec76de0
sulav97 <sulavkatuwal2@gmail.com>
2023-12-27 07:53:34 +0545

    Added new products

--- thrifty-frontend/src/data.js
+++ thrifty-frontend/src/data.js
@@ -16,4 +16,45 @@ export const imagesData = [
         imagePath: 'upload/20231226T132335824Z_jcq3fi_S22Black.jpg',
     },
     // Add more image data objects as needed
+
+    {
+        id: 3,
+        title: 'iPhone 12 Pro',
+        description: 'In very good condition ',
+        price: '1,20,000',
+        imagePath: 'uoppsdvhjzfuhyjsvcd',
+    },
+    {
+        id: 4,
+        title: 'iPhone 11 Red',
+        description: 'In very good condition ',
+        price: '1,20,000',
+        imagePath: 'uoppsdvhjzfuhyjsvcd',
+    },
+    {
+        id: 5,
+        title: 'Samsung 23Ultra',
+        description: 'In very good condition ',
+        price: '1,20,000',
+        imagePath: 'uoppsdvhjzfuhyjsvcd',
+    },
+    {
+        id: 6,
+        title: 'iPhone 14 Pro Max',
+        description: 'In very good condition ',
+        price: '1,20,000',
+        imagePath: 'uoppsdvhjzfuhyjsvcd',
+    },
+    {
+        id: 7,
+        title: 'iPhone 12 Pro',
+        description: 'In very good condition ',
+        price: '1,20,000',
+        imagePath: 'uoppsdvhjzfuhyjsvcd',
+    }
+
+
+
+
+
 ];
