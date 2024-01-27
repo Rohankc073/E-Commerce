cc7714374de0a064ba33c35489da4736e8b475a8
Rohankc073 <125729603+Rohankc073@users.noreply.github.com>
2024-01-27 10:29:38 +0545

    Some minor changes

--- thrifty-frontend/src/dev/previews.jsx
+++ thrifty-frontend/src/dev/previews.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox'
 import {PaletteTree} from './palette'
 import Footer from "../pages/footer";
+import Signup from "../pages/signup";
 
 const ComponentPreviews = () => {
     return (
         <Previews palette={<PaletteTree/>}>
             <ComponentPreview path="/Footer">
                 <Footer/>
+            </ComponentPreview>
+            <ComponentPreview path="/Signup">
+                <Signup/>
             </ComponentPreview>
         </Previews>
     )
